87390fff07588c7e66aa008ffbb475640424ae90
HannaSstrm <hanna.k.sjostrom@gmail.com>
2026-03-19 15:06:10 +0100

    Add files via upload

---
+++ dynamisk.c
@@ -0,0 +1,113 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct burgers_and_beer{
+	int burgers; 
+	int beer;
+} burgers_and_beer;
+
+void compute_burgers_and_beer(int m, int n, int t);
+void initialize_bb(burgers_and_beer bb, int t);
+
+int main (int argc, char *argv[], FILE **test_cases)
+{
+	FILE *test_cases = fopen(argv[1], "r");
+
+	if (argc == 1)
+	{
+		if (*test_cases == NULL)
+		{
+			fprintf(stderr, "Could not open the file: %s\n", argv[1]);
+			return 1;
+		}
+	}
+
+	char c;
+	int read_integers;
+	int m;
+	int n;
+	int t;
+
+	while ((c = fgetc(test_cases) != EOF))
+	{
+		if (c != ' ')
+		{
+			read_integers ++;
+			if (read_integers == 3)
+			{
+				t = atoi(&c);
+				results = calloc(t, sizeof(burgers_and_beer));
+				for (int i = 0; i < t; i++)
+				{
+					results[i] = compute_burgers_and_beer(m, n, t);
+				}
+				printf("%d %d", results[t-1].burger, results[t-1].beer);
+				free(results);
+				read_integers = 0;
+			}
+			else if (read_integers == 1)
+			{
+				m = atoi(&c);
+			}
+			else if (read_integers == 2)
+			{
+				n = atoi(&c);
+			}
+		}
+	}
+}
+
+void compute_burgers_and_beer(int m, int n, int t)
+{
+	burgers_and_beer option_m;
+	burgers_and_beer option_n;
+	initialize_bb(option_m, t);
+	initialize_bb(option_n, t);
+
+	if (t - m >= 0)
+	{
+		if (results[t-m] >= 0)
+		{
+			option_m = results[t-m]
+		}
+		option_m.burgers ++;
+	}
+
+	if (t - n >= 0)
+	{
+		if (results[t-n] >= 0)
+		{
+			option_n = results[t-n]
+		}
+		option_n.burgers ++;
+	}
+
+	if (option_m.burgers != 0 && option_n.burgers != 0)
+	{
+		if (option_m.beer == option_n.beer)
+		{
+			if (option_m.burgers > option_n.burgers)
+			{
+				results[t] = option_m;
+			}
+			else
+			{
+				results[t] = option_n;
+			}
+		}
+		else if (option_m.beer < option_n.beer)
+		{
+			results[t] = option_m;
+		}
+		else
+		{
+			results[t] = option_n;
+		}
+	}
+}
+
+void initialize_bb(burgers_and_beer bb, int t);
+{
+	bb.burgers = 0;
+	bb.beer = t;
+}
---
+++ memoisering.c
@@ -0,0 +1,123 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct burgers_and_beer{
+	int burgers; 
+	int beer;
+} burgers_and_beer;
+
+burgers_and_beer compute_burgers_and_beer(int m, int n, int t);
+void initialize_bb(burgers_and_beer bb, int t);
+
+int main (int argc, char *argv[], FILE **test_cases)
+{
+	FILE *test_cases = fopen(argv[1], "r");
+
+	if (argc == 1)
+	{
+		if (*test_cases == NULL)
+		{
+			fprintf(stderr, "Could not open the file: %s\n", argv[1]);
+			return 1;
+		}
+	}
+
+	burgers_and_beer *results;
+	burgers_and_beer result;
+	
+	char c;
+	int read_integers;
+	int m;
+	int n;
+	int t;
+
+	while ((c = fgetc(test_cases) != EOF))
+	{
+		if (c != ' ')
+		{
+			read_integers ++;
+			if (read_integers == 3)
+			{
+				t = atoi(&c);
+				results = malloc(sizeof(burgers_and_beer) * t);
+				for (int i = 0; i < t; i++)
+				{
+					results[i] = -1;
+				}
+				result = compute_burgers_and_beer(m, n, t);
+				printf("%d %d", result.burgers, result.beer);
+				free(results);
+				read_integers = 0;
+			}
+			else if (read_integers == 1)
+			{
+				m = atoi(&c);
+			}
+			else if (read_integers == 2)
+			{
+				n = atoi(&c);
+			}
+		}
+	}
+}
+
+burgers_and_beer compute_burgers_and_beer(int m, int n, int t)
+{
+	if (results[t] != -1)
+	{
+		return results[t];
+	}
+
+	burgers_and_beer option_m;
+	burgers_and_beer option_n;
+
+	initialize_bb(option_m, t);
+	initialize_bb(option_n, t);
+
+	if (t - m >= 0)
+	{
+		option_m.beer -= m;
+		option_m = compute_burgers_and_beer(m, n, t-m);
+		option_m.burgers ++;
+	}
+
+	if (t - n >= 0)
+	{
+		option_m.beer -= n;
+		option_m = compute_burgers_and_beer(m, n, t-n);
+		option_n.burgers ++;
+	}
+
+	if (option_m.burgers != 0 && option_n.burgers != 0)
+	{
+		if (option_m.beer == option_n.beer)
+		{
+			if (option_m.burgers > option_n.burgers)
+			{
+				results[t] = option_m;
+				return option_m;
+			}
+			else
+			{
+				results[t] = option_n;
+				return option_n;
+			}
+		}
+		else if (option_m.beer < option_n.beer)
+		{
+			results[t] = option_m;
+			return option_m;
+		}
+		else
+		{
+			results[t] = option_n;
+			return option_n;
+		}
+	}
+}
+
+void initialize_bb(burgers_and_beer bb, int t);
+{
+	bb.burgers = 0;
+	bb.beer = t;
+}
