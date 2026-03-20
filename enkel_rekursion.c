252e4b53541aa931983c2c1656666cc9350f85a3
Hanna <hanna.k.sjostrom@gmail.com>
2026-03-20 12:52:02 +0100

    fixat rekursiv

--- enkel_rekursion.c
+++ enkel_rekursion.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct burgers_and_beer{
 	int burgers; 
@@ -7,73 +8,77 @@ typedef struct burgers_and_beer{
 } burgers_and_beer;
 
 burgers_and_beer compute_burgers_and_beer(int m, int n, int t);
-void initialize_bb(burgers_and_beer bb, int t);
+burgers_and_beer initialize_bb(int t);
+void clear_string(char *string);
 
 int main (int argc, const char *argv[])
 {
 	FILE *test_cases = fopen(argv[1], "r");
 
-	if (argc == 1)
-	{
-		if (*test_cases == NULL)
-		{
-			fprintf(stderr, "Could not open the file: %s\n", argv[1]);
-			return 1;
-		}
-	}
-
 	burgers_and_beer result;
 	
 	char c;
-	int read_integers;
+	char s[6];
+	int read_integers = 1;
 	int m;
 	int n;
 	int t;
-	while ((c = fgetc(test_cases) != EOF))
+	do
 	{
-		if (c != ' ')
+		c = fgetc(test_cases);
+		if (c != ' ' && c != EOF)
 		{
-			read_integers ++;
+			s[strlen(s)] = c;
+		}
+		else
+		{
+			s[strlen(s)] = '\0';
 			if (read_integers == 3)
 			{
-				t = atoi(&c);
+				t = atoi(s);
 				result = compute_burgers_and_beer(m, n, t);
-				printf("%d %d", result.burgers, result.beer);
-				read_integers = 0;
+				printf("%d %d ", result.burgers, result.beer);
+				read_integers = 1;
+				clear_string(s);
 			}
 			else if (read_integers == 1)
 			{
-				m = atoi(&c);
+				m = atoi(s);
+				read_integers ++;
+				clear_string(s);
 			}
 			else if (read_integers == 2)
 			{
-				n = atoi(&c);
+				n = atoi(s);
+				read_integers ++;
+				clear_string(s);
 			}
 		}
-	}
+	} while (c != EOF);
 }
 
 burgers_and_beer compute_burgers_and_beer(int m, int n, int t)
 {
-	burgers_and_beer option_m;
-	burgers_and_beer option_n;
+	burgers_and_beer option_m = initialize_bb(t);
+	burgers_and_beer option_n = option_m;
 
-	initialize_bb(option_m, t);
-	initialize_bb(option_n, t);
-
+	if (t == 0)
+	{
+		return option_m;
+	}
+	
 	if (t - m >= 0)
 	{
-		option_m.beer -= m;
 		option_m = compute_burgers_and_beer(m, n, t-m);
-		option_m.burgers ++;
 	}
+	option_m.burgers ++;
+
 
 	if (t - n >= 0)
 	{
-		option_m.beer -= n;
-		option_m = compute_burgers_and_beer(m, n, t-n);
-		option_n.burgers ++;
+		option_n = compute_burgers_and_beer(m, n, t-n);
 	}
+	option_n.burgers ++;
 
 	if (option_m.burgers != 0 && option_n.burgers != 0)
 	{
@@ -99,12 +104,23 @@ burgers_and_beer compute_burgers_and_beer(int m, int n, int t)
 	}
 	else
 	{
-		
+
 	}
 }
 
-void initialize_bb(burgers_and_beer bb, int t)
+burgers_and_beer initialize_bb(int t)
 {
+	burgers_and_beer bb;
 	bb.burgers = 0;
 	bb.beer = t;
+	return bb;
+}
+
+void clear_string(char *string)
+{
+	int l = strlen(string);
+	for (int i = 0; i <= l; i++)
+	{
+		string[i] = '\0';
+	}
 }
