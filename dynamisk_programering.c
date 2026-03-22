c3baa3cd655ee0870b81f23b5e60c483c69e2053
Hanna <hanna.k.sjostrom@gmail.com>
2026-03-22 15:00:28 +0100

    gjort fil- och funktionsbeskrivningar

--- dynamisk_programering.c
+++ dynamisk_programering.c
@@ -1,3 +1,19 @@
+/*
+ * DV2 - 5DV204
+ * Spring 2026
+ * OU4 Algoritmiskt tänkande
+ *
+ * File:         dynamisk_programering.c
+ * Description:  A solution of the problem UVA - 10465 - Homer Simpson, using dynamical programming with tabulization
+ * Author:       Hanna Sjöström - ens24ham
+				 Rebecca Burström - ens24rbm
+ * Date:         22/03-2026
+ * Input:        An amount of test cases on the format "m n t\n" terminated by EOF, with m being the amount of time it takes to
+ *               eat a Krusty-burger, n being the amount of time it takes to eat a Kwik-e-Mart burger and t being the time
+ *               allocated to eat the burgers
+ * Output:       Prints the ideal amount of burgers that can be eaten and the neccesary remaining beer time for each test case
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -33,6 +49,12 @@ int main (int argc, const char *argv[])
 	return 0;
 }
 
+/* Description: Computes the ideal amount of burgers that can be eaten in t minutes 
+ * if the time taken to eat the burgers is m and n
+ * Input: The time it takes to eat a Krusty-burger, the time it takes to eat a Kwik-e-Mart-burger 
+ * and the time alloted for eating burgers in minutes
+ * Output: Returns the ideal amount of burgers and remaining beer time.
+ */
 burgers_and_beer compute_burgers_and_beer(int m, int n, int t)
 {
 	burgers_and_beer results[t+1];
@@ -50,7 +72,8 @@ burgers_and_beer compute_burgers_and_beer(int m, int n, int t)
 
 		if (i == 0)
 		{
-			put_result_in_array(results, option_m, i);
+			//0 burgers and t beer time is set as result, since no burgers can be eaten
+			put_result_in_array(results, option_m, i); 
 		}
 		if (i - m >= 0)
 		{
@@ -92,6 +115,10 @@ burgers_and_beer compute_burgers_and_beer(int m, int n, int t)
 	return results[t];
 }
 
+/* Description: Returns an burger_and_beer structure in which the amount of burgers is set to 0 and time set to t
+ * Input: The time which is to be inserted as beer time in the struct.
+ * Output: Returns the created and initialized struct.
+ */
 burgers_and_beer initialize_bb(int t)
 {
 	burgers_and_beer bb;
@@ -100,6 +127,10 @@ burgers_and_beer initialize_bb(int t)
 	return bb;
 }
 
+/* Description: Sets the given option as the result in the given array at index t
+ * Input: The result array in which the option is to be set as result, the option and the index
+ * Output: An array in which the result has been set at index t
+ */
 void set_result(burgers_and_beer *results, burgers_and_beer option, int t)
 {
 	results[t].burgers = option.burgers;
--- enkel_rekursion.c
+++ enkel_rekursion.c
@@ -1,3 +1,19 @@
+/*
+ * DV2 - 5DV204
+ * Spring 2026
+ * OU4 Algoritmiskt tänkande
+ *
+ * File:         enkel_rekursion.c
+ * Description:  A solution of the problem UVA - 10465 - Homer Simpson, using simple recursion
+ * Author:       Hanna Sjöström - ens24ham
+				 Rebecca Burström - ens24rbm
+ * Date:         22/03-2026
+ * Input:        An amount of test cases on the format "m n t\n" terminated by EOF, with m being the amount of time it takes to
+ *               eat a Krusty-burger, n being the amount of time it takes to eat a Kwik-e-Mart burger and t being the time
+ *               allocated to eat the burgers
+ * Output:       Prints the ideal amount of burgers that can be eaten and the neccesary remaining beer time for each test case
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -30,6 +46,12 @@ int main (int argc, const char *argv[])
 	return 0;
 }
 
+/* Description: Computes the ideal amount of burgers that can be eaten in t minutes 
+ * if the time taken to eat the burgers is m and n
+ * Input: The time it takes to eat a Krusty-burger, the time it takes to eat a Kwik-e-Mart-burger 
+ * and the time alloted for eating burgers in minutes
+ * Output: Returns the ideal amount of burgers and remaining beer time.
+ */
 burgers_and_beer compute_burgers_and_beer(int m, int n, int t)
 {
 	burgers_and_beer option_m = initialize_bb(t);
@@ -37,6 +59,7 @@ burgers_and_beer compute_burgers_and_beer(int m, int n, int t)
 
 	if (t == 0)
 	{
+		//0 burgers and t beer time is returned, since no burgers can be eaten
 		return option_m;
 	}
 	
@@ -77,6 +100,10 @@ burgers_and_beer compute_burgers_and_beer(int m, int n, int t)
 	}
 }
 
+/* Description: Returns an burger_and_beer structure in which the amount of burgers is set to 0 and time set to t
+ * Input: The time which is to be inserted as beer time in the struct.
+ * Output: Returns the created and initialized struct.
+ */
 burgers_and_beer initialize_bb(int t)
 {
 	burgers_and_beer bb;
